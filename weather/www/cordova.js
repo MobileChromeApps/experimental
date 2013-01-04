5ca5a806c1208ce46718a68909be24153a10257e
Michal Mocny <mmocny@gmail.com>
2013-01-04 14:17:13 -0500

    changes to new mobile chrome app structure

--- weather/www/cordova.js
+++
@@ -1,5 +0,0 @@
-var scripts = document.getElementsByTagName('script');
-var platform = navigator.userAgent.indexOf('Android') >= 0 ? 'android' : 'ios';
-var cordovaPath = scripts[scripts.length - 1].src.replace('cordova.js', 'cordova.' + platform + '.js');
-
-document.write('<script type="text/javascript" charset="utf-8" src="' + cordovaPath + '"></script>');
